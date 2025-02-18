9fde96c3f10752aea100a0bc0c973d4a25b0178b
Ospinajuanp <juanpo996@gmail.com>
2025-02-18 17:37:11 -0500

    Init section Sample

---
+++ src/Components/Sample/Sample.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sample.css';
+
+const Sample = () => {
+    return (
+        <div className='Sample'>
+            b
+        </div>
+    );
+};
+
+export default Sample;
